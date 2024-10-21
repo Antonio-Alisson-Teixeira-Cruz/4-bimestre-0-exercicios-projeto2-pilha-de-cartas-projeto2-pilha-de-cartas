58a49a4cdfa58ae8f76b07efbb7d3bbf5000ec5d
Antonio-Alisson-Teixeira-Cruz <AntonioAlissonTeixeiraCruz@gmail.com>
2024-10-21 16:56:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+let deck = []
+let menu = ``
+let interact = false
+let card = ''
+let leftCard
 
+
+do{
+    menu = ``
+        menu += `Cartas  no deck: ${deck.length}.\n`
+        menu += `1- Adicionar carta.\n`
+        menu += `2- Retirar.\n`
+        menu += `3- Sair.`
+
+    interact = prompt(menu)
+
+    switch(interact){
+        case '1':
+            card = prompt('Qual carta deseja adicionar?')
+            deck.push(card)
+            break
+        
+        case '2':
+            if(deck.length == 0 ){
+                alert('Sem cards no baralho')
+            } else {
+                leftCard = deck.pop()
+                alert(`A carta retirada foi ${leftCard}`)
+            }
+            break
+        case '3':
+            alert('Encerrando...')
+            break
+        default:
+            alert('Encerrando...')
+
+        
+    }
+
+} while (interact != '3')
